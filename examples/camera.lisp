f1f9dad9f16f37167ff4a3f4b5fcb21748e55ea6
Quentin Stievenart <acieroid@awesom.eu>
2011-01-12 21:08:44 +0100

    using simple-app in camera example, adapting it with new rotation api

--- examples/camera.lisp
+++ examples/camera.lisp
@@ -1,48 +1,33 @@
 (require :scelisp)
 (in-package :scelisp)
 
-(defparameter *width* 800)
-(defparameter *height* 600)
-
-(defclass app (sce)
-  ((scene :accessor scene)
-   (model :accessor model)
+(defclass app (simple-app)
+  ((model :accessor model)
    (rx :accessor rx)
    (ry :accessor ry)))
 
 (defmethod init ((app app))
-  (let* ((camera (make-instance 'camera :width *width* :height *height*))
-         (red-light (make-instance 'light :color '(0.8 0.2 0.2)
-                                   :x 1.0 :y 2.4 :z 1.0))
-         (green-light (make-instance 'light :color '(0.2 0.8 0.2)
-                                     :x 1.0 :y -2.4 :z 1.0))
-         (model (make-instance 'model
+  (let* ((model (make-instance 'model
                                :mesh (make-instance 'mesh :file "cube.obj")))
-         (scene (make-instance 'scene :camera camera))
          (rx (make-instance 'inert :coeff 0.1 :accum t))
          (ry (make-instance 'inert :coeff 0.1 :accum t)))
-    (setf (scene app) scene
-          (model app) model
+    (setf (model app) model
           (rx app) rx
           (ry app) ry)
-    (add scene red-light)
-    (add scene green-light)
-    (add scene model)
-    (sce-matrix4-mulscale (get-matrix model) 0.3 0.3 0.3)))
+    (add (scene app) model)
+    (translate (camera (scene app)) 0.0 0.0 -1.0)))
 
 (defmethod update ((app app))
-  (update (scene app))
-  (compute (rx app))
-  (compute (ry app))
-  (let ((matrix (get-matrix (model app))))
-    (translate (model app) 0.0 0.0 0.0)
-    (sce-matrix4-mulscale matrix 0.3 0.3 0.3)
-    (sce-matrix4-mulrotx matrix (* (value (rx app)) 0.01))
-    (sce-matrix4-mulrotz matrix (* (value (ry app)) 0.01)))
-  (has-moved (get-node (model app))))
-
-(defmethod draw ((app app))
-  (draw (scene app)))
+  (with-accessors ((scene scene) (model model)
+                   (rx rx) (ry ry))
+      app
+    (compute rx)
+    (compute ry)
+    (set-position model 0.0 0.0 0.0)
+    (scale model 0.3 0.3 0.3)
+    (rot-x model (* (value (rx app)) 0.01))
+    (rot-z model (* (value (ry app)) 0.01))
+    (has-moved (get-node (model app)))))
 
 (defmethod handle-event ((app app) (event (eql :mouse-motion-event))
                          &key state x-rel y-rel)
@@ -51,4 +36,4 @@
     (operate (rx app) #'+ y-rel)))
 
 (defun main ()
-  (launch (make-instance 'app :width *width* :height *height*)))
+  (launch (make-instance 'app)))
