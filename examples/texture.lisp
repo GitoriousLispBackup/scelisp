1a54d7998aefbb8e9e90325d6f9af84835bcc8f3
Quentin Stievenart <acieroid@awesom.eu>
2011-01-12 22:41:10 +0100

    updating texture example

--- examples/texture.lisp
+++ examples/texture.lisp
@@ -7,25 +7,16 @@
   ((tex :accessor app)))
 
 (defmethod init ((app tex-app))
-  (let* ((camera (make-instance 'camera :width *width* :height *height*))
-         (red-light (make-instance 'light :color '(0.8 0.2 0.2)
-                                   :x 1.0 :y 2.4 :z 1.0))
-         (green-light (make-instance 'light :color '(0.2 0.8 0.2)
-                                     :x 1.0 :y -2.4 :z 1.0))
-         (model (make-instance 'model
+  (let* ((model (make-instance 'model
                                :mesh (make-instance 'mesh :file "cube.obj")
                                :texture (make-instance 'texture :file "lisp.png")))
-         (scene (make-instance 'scene :camera camera))
          (rx (make-instance 'inert :coeff 0.1 :accum t))
          (ry (make-instance 'inert :coeff 0.1 :accum t)))
-    (setf (scene app) scene
-          (model app) model
+    (setf (model app) model
           (rx app) rx
           (ry app) ry)
-    (add scene red-light)
-    (add scene green-light)
-    (add scene model)
-    (sce-matrix4-mulscale (get-matrix model) 0.3 0.3 0.3)))
+    (add (scene app) model)
+    (translate (camera (scene app)) 0.0 0.0 -1.0)))
 
 (defun main ()
   (launch (make-instance 'tex-app)))
