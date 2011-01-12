48d1f4939fbb0b423953f47a77ddd5cf49119d4e
Quentin Stievenart <acieroid@awesom.eu>
2011-01-12 22:38:04 +0100

    updating model1 example

--- examples/model1.lisp
+++ examples/model1.lisp
@@ -2,30 +2,15 @@
 (require :scelisp)
 (in-package :scelisp)
 
-(defparameter *width* 800)
-(defparameter *height* 600)
+(defclass model-app (simple-app)
+  ((model :accessor model)))
 
-(defclass app (sce)
-  ((scene :accessor scene)
-   (model :accessor model)))
-
-(defmethod init ((app app))
-  (let* ((camera (make-instance 'camera :width *width* :height *height*))
-         (light (make-instance 'light :x 1.0 :y 2.4 :z 1.0))
-         (model (make-instance 'model
-                               :mesh (make-instance 'mesh :file "cube.obj")))
-         (scene (make-instance 'scene :camera camera)))
-    (setf (scene app) scene
-          (model app) model)
-    (add scene model)
-    (add scene light)
-    (sce-matrix4-mulscale (get-matrix model) 0.3 0.3 0.3)))
-
-(defmethod update ((app app))
-  (update (scene app)))
-
-(defmethod draw ((app app))
-  (draw (scene app)))
+(defmethod init ((app model-app))
+  (let* ((model (make-instance 'model
+                               :mesh (make-instance 'mesh :file "cube.obj"))))
+    (setf (model app) model)
+    (add (scene app) model)
+    (set-position model 0.0 0.0 -2.0)))
 
 (defun main ()
-  (launch (make-instance 'app :width *width* :height *height*)))
+  (launch (make-instance 'model-app)))
